53979837c13bc42b38881dff0d816c5573d20043
Ceyda Dereci <ceyda.dereci@ug.bilkent.edu.tr>
2020-12-20 22:38:36 +0300

    Part c is added.

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,4 @@
 public class Node {
     String data;
     Node next;
-
 }
---
+++ src/SimpleLinkedList.java
@@ -0,0 +1,29 @@
+public class SimpleLinkedList {
+
+
+
+    public SimpleLinkedList(){
+
+
+    }
+
+    public void addToTail(String data){
+
+    }
+
+    public String removeFromHead(){
+        return "";
+    }
+
+    public boolean isEmpty(){
+
+        return true;
+    }
+
+    public Node get(String data){
+        return;
+    }
+    public String toString(){
+        return "";
+    }
+}
